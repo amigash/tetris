03eb8b20e518ad6dfbd698ad329e2c7fe6bfe130
Addison Migash <tuo20482@temple.edu>
2023-11-22 13:26:44 -0500

    Added random generation for Tetromino. No longer initializes an array of all members

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,4 +1,4 @@
-/// https://tetris.wiki/Super_Rotation_System
+/// <https://tetris.wiki/Super_Rotation_System>
 type Matrix<const N: usize> = [[bool; N]; N];
 
 const L: [Matrix<3>; 4] = [
@@ -12,7 +12,6 @@ const L: [Matrix<3>; 4] = [
         [false, true, false],
         [false, true, true],
     ],
-
     [
         [false, false, false],
         [true, true, true],
@@ -47,10 +46,7 @@ const J: [Matrix<3>; 4] = [
         [true, true, false],
     ],
 ];
-const O: Matrix<2> = [
-        [true, true],
-        [true, true],
-];
+const O: Matrix<2> = [[true, true], [true, true]];
 
 const I: [Matrix<4>; 4] = [
     [
@@ -148,53 +144,30 @@ const S: [Matrix<3>; 4] = [
     ],
 ];
 pub enum Tetromino {
-    O {
-        matrix: Matrix<2>
-    },
-    I {
-        rotation: [Matrix<4>; 4]
-    },
-    L {
-        rotation: [Matrix<3>; 4]
-    },
-    J {
-        rotation: [Matrix<3>; 4]
-    },
-    T {
-        rotation: [Matrix<3>; 4]
-    },
-    Z {
-        rotation: [Matrix<3>; 4]
-    },
-    S {
-        rotation: [Matrix<3>; 4]
-    },
+    O { matrix: Matrix<2> },
+    I { rotation: [Matrix<4>; 4] },
+    L { rotation: [Matrix<3>; 4] },
+    J { rotation: [Matrix<3>; 4] },
+    T { rotation: [Matrix<3>; 4] },
+    Z { rotation: [Matrix<3>; 4] },
+    S { rotation: [Matrix<3>; 4] },
 }
 
-impl Tetromino {
-    pub fn initialize() -> [Tetromino; 7] {
-        [
-            Tetromino::O {
-                matrix: O
-            },
-            Tetromino::I {
-                rotation: I
-            },
-            Tetromino::L {
-                rotation: L
-            },
-            Tetromino::J {
-                rotation: J
-            },
-            Tetromino::T {
-                rotation: T
-            },
-            Tetromino::Z {
-                rotation: Z
-            },
-            Tetromino::S {
-                rotation: S
-            },
-        ]
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+impl Distribution<Tetromino> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Tetromino {
+        match rng.gen_range(0..=6) {
+            0 => Tetromino::O { matrix: O },
+            1 => Tetromino::I { rotation: I },
+            2 => Tetromino::L { rotation: L },
+            3 => Tetromino::J { rotation: J },
+            4 => Tetromino::T { rotation: T },
+            5 => Tetromino::Z { rotation: Z },
+            6 => Tetromino::S { rotation: S },
+            _ => unreachable!(),
+        }
     }
 }
