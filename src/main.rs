fe2162fceb675d32f8022d34d662addc73c39f6f
Addison Migash <tuo20482@temple.edu>
2023-11-25 17:39:48 -0500

    Blocks is now stored in a vector rather than a HashSet. Blocks now store color data and have their own struct. Rows now disappear when filled and shift down the blocks above them. Added field outline to rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use macroquad::prelude::*;
 use macroquad_canvas::Canvas2D;
-use std::collections::HashSet;
 use tetromino::Tetromino;
 
 mod tetromino;
@@ -11,9 +10,9 @@ const HEIGHT: usize = 20;
 #[macroquad::main("Tetris")]
 async fn main() {
     let mut game = Game::new();
-    let mut frame: u8 = 0;
+    let mut frame: u32 = 0;
     loop {
-        while get_time() < f64::from(frame + 1) / 5.0 {
+        while get_time() < f64::from(frame + 1) / 3.0 {
             game.update_projection();
             game.handle_input();
             game.render();
@@ -23,9 +22,21 @@ async fn main() {
         frame += 1;
     }
 }
+#[derive(Copy, Clone)]
+struct Block {
+    x: usize,
+    y: usize,
+    color: u32,
+}
+
+impl PartialEq for Block {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
 
 struct Game {
-    blocks: HashSet<[usize; 2]>,
+    blocks: Vec<Block>,
     dx: isize,
     dy: usize,
     projection_dy: usize,
@@ -35,15 +46,18 @@ struct Game {
 }
 
 impl Game {
-
-    fn block(&self, y: usize, x: usize) -> [usize; 2] {
-        [y + self.dy, x.saturating_add_signed(self.dx)]
+    fn block(&self, y: usize, x: usize) -> Block {
+        Block {
+            x: x.saturating_add_signed(self.dx),
+            y: y + self.dy,
+            color: self.tetromino.color,
+        }
     }
     fn new() -> Self {
         let tetromino = Tetromino::random();
         Self {
-            blocks: HashSet::new(),
-            dx: 0,
+            blocks: Vec::new(),
+            dx: (WIDTH / 2 - 1) as isize,
             dy: 0,
             projection_dy: 0,
             tetromino,
@@ -52,17 +66,21 @@ impl Game {
         }
     }
 
-    fn update(&mut self) {
-        if self.dy != self.projection_dy {
-            self.dy += 1;
-            return
-        }
-        self.place();
+    fn next_tetromino(&mut self) {
         let tetromino = Tetromino::random();
         self.tetromino = tetromino;
         self.projection = tetromino;
         self.dy = 0;
-        self.dx = 0;
+        self.dx = (WIDTH / 2 - 1) as isize;
+    }
+
+    fn update(&mut self) {
+        if self.dy == self.projection_dy {
+            self.place();
+            self.next_tetromino();
+        } else {
+            self.dy += 1;
+        }
     }
 
     fn update_projection(&mut self) {
@@ -76,7 +94,24 @@ impl Game {
     }
 
     fn drop(&mut self) {
-        self.dy = self.projection_dy
+        self.dy = self.projection_dy;
+        self.update();
+    }
+
+    fn is_row_full(&self, row: usize) -> bool {
+        self.blocks.iter().filter(|&&block| block.y == row).count() == WIDTH
+    }
+
+    fn remove_rows(&mut self, rows: Vec<usize>) {
+        self.blocks.retain(|block| !rows.contains(&block.y));
+
+        for row in rows {
+            for block in &mut self.blocks {
+                if block.y < row {
+                    block.y += 1;
+                }
+            }
+        }
     }
 
     fn handle_input(&mut self) {
@@ -117,50 +152,70 @@ impl Game {
 
     fn is_horizontal_collision(&self) -> bool {
         self.tetromino.blocks().iter().any(|&[y, x]| {
-            x.wrapping_add_signed(self.dx) > WIDTH || self.blocks.contains(&self.block(y, x))
+            x.wrapping_add_signed(self.dx) >= WIDTH || self.blocks.contains(&self.block(y, x))
         })
     }
 
     fn is_vertical_collision(&self) -> bool {
-        self.tetromino.blocks().iter().any(|&[y, x]| {
-            self.blocks.contains(&self.block(y, x))
-                || y + self.dy > HEIGHT
-        })
+        self.tetromino
+            .blocks()
+            .iter()
+            .any(|&[y, x]| self.blocks.contains(&self.block(y, x)) || y + self.dy >= HEIGHT)
     }
 
     fn place(&mut self) {
+        let mut rows = Vec::new();
         for [y, x] in self.tetromino.blocks() {
-            self.blocks.insert(self.block(y, x));
+            self.blocks.push(self.block(y, x));
+            rows.push(y + self.dy);
         }
+        rows.sort_unstable();
+        rows.dedup();
+        rows.retain(|&row| self.is_row_full(row));
+        self.remove_rows(rows);
     }
 
     fn render(&self) {
-        fn draw_block(block: [usize; 2]) {
-            let [y, x] = block;
-            draw_rectangle(x as f32 * 20.0, y as f32 * 20.0, 20.0, 20.0, RED);
+        fn draw_block(block: Block, color: Color) {
+            draw_rectangle(
+                block.x as f32 * 20.0,
+                block.y as f32 * 20.0,
+                20.0,
+                20.0,
+                color,
+            );
         }
 
         set_camera(&self.canvas.camera);
         clear_background(WHITE);
 
         for &[y, x] in &self.projection.blocks() {
-            let [y, x] = [y + self.projection_dy, x.saturating_add_signed(self.dx)];
-            draw_rectangle(
-                x as f32 * 20.0,
-                y as f32 * 20.0,
-                20.0,
-                20.0,
-                Color::from_rgba(255, 0, 0, 100)
-            );
+            let mut color = Color::from_hex(self.projection.color);
+            color.a = 0.1;
+            let block = Block {
+                x: x.saturating_add_signed(self.dx),
+                y: y + self.projection_dy,
+                color: self.projection.color,
+            };
+            draw_block(block, color);
         }
 
         for &block in &self.blocks {
-            draw_block(block);
+            draw_block(block, Color::from_hex(block.color));
         }
 
         for &[y, x] in &self.tetromino.blocks() {
-            draw_block(self.block(y, x));
+            draw_block(self.block(y, x), Color::from_hex(self.tetromino.color));
         }
+
+        draw_rectangle_lines(
+            0.0,
+            0.0,
+            WIDTH as f32 * 20.0,
+            HEIGHT as f32 * 20.0,
+            2.0,
+            BLACK,
+        );
 
         set_default_camera();
         clear_background(BLACK);
